14c17cd5cc0a6a7917ddf0053dfffe5d03796bad
Kirat Arora <kiratarora@umass.edu>
2024-07-17 15:38:21 +0530

    spelling error

--- embedder.py
+++ embedder.py
@@ -1,5 +1,7 @@
-# This file is the implemnetation for the BaseEmbedding class to incorporate custom embeding
 from InstructorEmbedding import INSTRUCTOR
+
+# This file is the implemnetation for the BaseEmbedding class to incorporate custom embeding
+
 model = INSTRUCTOR('hkunlp/instructor-large')
 sentence = "3D ActionSLAM: wearable person tracking in multi-floor environments"
 instruction = "Represent the Science title:"
